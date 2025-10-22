5e2e21d04e81daf91fdea7f9ff2729e228ffbb38
awanat-olawale <olawaleawanat05@gmail.com>
2025-10-22 22:00:41 +0100

    Add stage_1/analyzer app for string analysis endpoints

--- stage_1/analyzer/urls.py
+++ stage_1/analyzer/urls.py
@@ -1,7 +1,19 @@
+from django.http import JsonResponse
 from django.urls import path
 from .views import analyze_string, get_string, get_all_strings, delete_string, filter_by_natural_language
 
+def home(request):
+    return JsonResponse({
+        "message": "Welcome to HNG Stage 1 String Analyzer API!",
+        "endpoints": [
+            "/strings/",
+            "/strings/all/",
+            "/strings/filter-by-natural-language/",
+        ]
+    })
+
 urlpatterns = [
+    path('', home, name='home'),
     path('strings/', analyze_string, name='analyze_string'),
     path('strings/get/', get_string, name='get_string'),
     path('strings/all/', get_all_strings, name='get_all_strings'),
